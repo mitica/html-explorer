18bcf91e4678e59e7448df84624cf46ef5d915b6
Dumitru Cantea <canteadumitru@gmail.com>
2015-08-03 12:09:53 +0300

    better decode page, content filter

--- test/explorer-test.js
+++ test/explorer-test.js
@@ -15,4 +15,17 @@ describe('explorer', function() {
 			assert.equal('Взрывное устройство в гостинице «Radisson Славянская» в центре Москвы не найдено', result.title);
 		});
 	});
+	it('explore charset=iso-8859-2', function() {
+		return explorer.explore('http://www.wprost.pl/ar/516408/w-polsce-najwiecej-wydaja-turysci-amerykanscy-ale-najwiecej-jest-niemieckich/', {
+			images: {
+				identify: true,
+				timeout: 300
+			},
+			content: {
+			}
+		}).then(function(result) {
+			// console.log(result);
+			assert.equal('W Polsce najwięcej wydają turyści amerykańscy, ale najwięcej jest niemieckich', result.title);
+		});
+	});
 });
